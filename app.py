df8c9f65c9243a9fb9dbf7f9d36cb2310529d508
Hema9121 <hemasrinivasulu.ds@gmail.com>
2024-02-16 18:52:33 +0530

    third commit

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from Housing.logger import logging
+
 
 app=Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
+    logging.info("we are testing logging module!!!!")
     return "hello world!!!!"
 
 if __name__=="__main__":
