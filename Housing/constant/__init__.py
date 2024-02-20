8e37f3f7bdfbdb100fcd7619f5e06cd87d3e4e2c
Hema9121 <hemasrinivasulu.ds@gmail.com>
2024-02-20 20:44:34 +0530

    last commit

--- Housing/constant/__init__.py
+++ Housing/constant/__init__.py
@@ -79,3 +79,5 @@ MODEL_PATH_KEY = "model_path"
 MODEL_PUSHER_CONFIG_KEY="model_pusher_config"
 MODEL_PUSHER_EXPORT_DIR_KEY="model_export_dir"
 
+EXPERIMENT_DIR_NAME="experiment"
+EXPERIMENT_FILE_NAME="experiment.csv"
--- Housing/pipeline/pipeline.py
+++ Housing/pipeline/pipeline.py
@@ -1,6 +1,6 @@
 from Housing.exception import HousingException
 from Housing.logger import logging
-import sys
+import sys,os
 
 from Housing.components.DataIngestion import DataIngestion
 from Housing.components.DataValidation import DataValidation
@@ -13,10 +13,30 @@ from Housing.config.configuration import Configuration
 from Housing.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig,ModelEvaluationConfig,ModelPusherConfig
 from Housing.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact,DataTransformationArtifact,ModelTrainerArtifact,ModelEvaluationArtifact,ModelPusherArtifact
 
-class Pipeline:
+from collections import namedtuple
+from datetime import datetime
+import uuid
+from threading import Thread
+from typing import List
+from multiprocessing import Process
+import pandas as pd
+from Housing.constant import EXPERIMENT_DIR_NAME, EXPERIMENT_FILE_NAME
+
+Experiment = namedtuple("Experiment", ["experiment_id", "initialization_timestamp", "artifact_time_stamp",
+                                       "running_status", "start_time", "stop_time", "execution_time", "message",
+                                       "experiment_file_path", "accuracy", "is_model_accepted"])
+
+
+class Pipeline(Thread):
     def __init__(self,config:Configuration=Configuration())->None:
+        experiment: Experiment = Experiment(*([None] * 11))
+        experiment_file_path = None
 
         try:
+            os.makedirs(config.training_pipeline_config.artifact_dir, exist_ok=True)
+            Pipeline.experiment_file_path=os.path.join(config.training_pipeline_config.artifact_dir,EXPERIMENT_DIR_NAME, EXPERIMENT_FILE_NAME)
+            super().__init__(daemon=False, name="pipeline")
+            self.config = config
             self.config=config
 
         except Exception as e:
@@ -84,6 +104,30 @@ class Pipeline:
     
     def run_pipeline(self):
         try:
+            if Pipeline.experiment.running_status:
+                logging.info("Pipeline is already running")
+                return Pipeline.experiment
+            
+            logging.info("Pipeline starting.")
+
+            experiment_id = str(uuid.uuid4())
+
+            Pipeline.experiment = Experiment(experiment_id=experiment_id,
+                                             initialization_timestamp=self.config.time_stamp,
+                                             artifact_time_stamp=self.config.time_stamp,
+                                             running_status=True,
+                                             start_time=datetime.now(),
+                                             stop_time=None,
+                                             execution_time=None,
+                                             experiment_file_path=Pipeline.experiment_file_path,
+                                             is_model_accepted=None,
+                                             message="Pipeline has been started.",
+                                             accuracy=None,
+                                             )
+            logging.info(f"Pipeline experiment: {Pipeline.experiment}")
+
+            self.save_experiment()
+
             data_ingestion_artifact=self.start_data_ingestion()
             data_validation_artifact=self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact=self.start_data_transformation(data_ingestion_artifact=data_ingestion_artifact,
@@ -99,18 +143,53 @@ class Pipeline:
                 logging.info("Trained model rejected.")
             logging.info("Pipeline completed.")
 
+            stop_time = datetime.now()
+            Pipeline.experiment = Experiment(experiment_id=Pipeline.experiment.experiment_id,
+                                             initialization_timestamp=self.config.time_stamp,
+                                             artifact_time_stamp=self.config.time_stamp,
+                                             running_status=False,
+                                             start_time=Pipeline.experiment.start_time,
+                                             stop_time=stop_time,
+                                             execution_time=stop_time - Pipeline.experiment.start_time,
+                                             message="Pipeline has been completed.",
+                                             experiment_file_path=Pipeline.experiment_file_path,
+                                             is_model_accepted=model_evaluation_artifact.is_model_accepted,
+                                             accuracy=model_trainer_artifact.model_accuracy
+                                             )
+            logging.info(f"Pipeline experiment: {Pipeline.experiment}")
+            self.save_experiment()
+
 
         except Exception as e:
             raise HousingException(e,sys) from e
         
 
 
+    def run(self):
+        try:
+            self.run_pipeline()
+        except Exception as e:
+            raise e
 
+    def save_experiment(self):
+        try:
+            if Pipeline.experiment.experiment_id is not None:
+                experiment = Pipeline.experiment
+                experiment_dict = experiment._asdict()
+                experiment_dict: dict = {key: [value] for key, value in experiment_dict.items()}
 
-"""
-try:
+                experiment_dict.update({
+                    "created_time_stamp": [datetime.now()],
+                    "experiment_file_path": [os.path.basename(Pipeline.experiment.experiment_file_path)]})
 
-except Exception as e:
-raise HousingException(e,sys) from e
+                experiment_report = pd.DataFrame(experiment_dict)
 
-"""
+                os.makedirs(os.path.dirname(Pipeline.experiment_file_path), exist_ok=True)
+                if os.path.exists(Pipeline.experiment_file_path):
+                    experiment_report.to_csv(Pipeline.experiment_file_path, index=False, header=False, mode="a")
+                else:
+                    experiment_report.to_csv(Pipeline.experiment_file_path, mode="w", index=False, header=True)
+            else:
+                print("First start experiment")
+        except Exception as e:
+            raise HousingException(e, sys) from e
