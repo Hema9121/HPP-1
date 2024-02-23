92cd8b6070813e5feaef049415e35fb148f4ab1c
Hema9121 <hemasrinivasulu.ds@gmail.com>
2024-02-23 20:48:40 +0530

    last

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 from typing import List
 
 PROJECT_NAME="HPP"
-VERSION="0.0.11"
+VERSION="0.0.24"
 AUTHOR="Hema Srinivasulu"
 DESRCIPTION="This is Machine Learning Project"
 
