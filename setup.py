344b327f3cf11a8b961af015ba89312cf3daf81e
Hema9121 <hemasrinivasulu.ds@gmail.com>
2024-02-28 15:55:14 +0530

    housing

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 from typing import List
 
 PROJECT_NAME="HPP"
-VERSION="0.0.004"
+VERSION="0.0.80"
 AUTHOR="Hema Srinivasulu"
 DESRCIPTION="This is Machine Learning Project"
 
